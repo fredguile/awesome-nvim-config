13e7e74238aa1b2ca5e27601b39130d83cbe2d7c
Fred Ghilini <fredguile@gmail.com>
2026-05-04 22:48:59 +0200

    Added shortcut

--- lua/plugins/git-diffview.lua
+++ lua/plugins/git-diffview.lua
@@ -4,6 +4,7 @@ return {
 		cmd = { "DiffviewOpen", "DiffviewFileHistory" },
 		keys = {
 			{ "<leader>gd", "<cmd>DiffviewOpen<CR>", desc = "Diff View" },
+			{ "<leader>ga", "<cmd>DiffviewToggleFiles<CR>", desc = "Diff Toggle Files" },
 			{ "<leader>gh", "<cmd>DiffviewFileHistory<CR>", desc = "Diff File History" },
 		},
 	},
