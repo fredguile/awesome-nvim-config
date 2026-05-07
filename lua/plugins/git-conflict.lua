ff19852f6c0aa3f9a7f713c87fa39ac13da595e2
Fred Ghilini <fredguile@gmail.com>
2026-05-07 10:45:46 +0200

    Fixed compatibility issue between git-conflict and diffview.nvim

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -3,20 +3,21 @@ return {
 	lazy = false,
 	config = function()
 		local git_conflict = require("git-conflict")
+		local api = vim.api
 
 		git_conflict.setup({
 			default_mappings = true,
 			default_commands = true,
 			list_opener = "copen",
-			highlights = { -- They must have background color, otherwise the default color will be used
+			highlights = {
 				incoming = "DiffAdd",
 				current = "DiffText",
 			},
 		})
 
-		local group = vim.api.nvim_create_augroup("GitConflictRefresh", { clear = true })
+		local group = api.nvim_create_augroup("GitConflictRefresh", { clear = true })
 		local function refresh_and_clear(bufnr)
-			if bufnr and vim.api.nvim_buf_is_valid(bufnr) then
+			if bufnr and api.nvim_buf_is_valid(bufnr) then
 				pcall(git_conflict.clear, bufnr)
 			end
 			if vim.fn.exists(":GitConflictRefresh") == 2 then
@@ -24,21 +25,49 @@ return {
 			end
 		end
 
-		vim.api.nvim_create_autocmd({ "BufWritePost", "BufDelete" }, {
+		api.nvim_create_autocmd({ "BufWritePost", "BufDelete" }, {
 			group = group,
 			callback = function(args)
 				refresh_and_clear(args.buf)
 			end,
 		})
 
-		vim.api.nvim_create_autocmd("User", {
+		api.nvim_create_autocmd("User", {
 			pattern = { "DiffviewFileOpened", "DiffviewPanelTOGGLE" },
 			callback = function()
 				vim.schedule(function()
-					local buf = vim.api.nvim_get_current_buf()
-					if vim.api.nvim_buf_is_valid(buf) then
+					local buf = api.nvim_get_current_buf()
+					if api.nvim_buf_is_valid(buf) then
 						pcall(git_conflict.clear, buf)
 					end
+					pcall(function()
+						api.nvim_set_decoration_provider(git_conflict.NAMESPACE, {
+							on_buf = function()
+								return false
+							end,
+						})
+					end)
+				end)
+			end,
+		})
+
+		api.nvim_create_autocmd("User", {
+			pattern = { "DiffviewExit" },
+			callback = function()
+				vim.schedule(function()
+					pcall(function()
+						local utils = require("git-conflict.utils")
+						api.nvim_set_decoration_provider(git_conflict.NAMESPACE, {
+							on_buf = function(_, bufnr, _)
+								return utils.is_valid_buf(bufnr)
+							end,
+							on_win = function(_, _, bufnr, _, _)
+								if git_conflict.visited_buffers[bufnr] then
+									git_conflict.process(bufnr)
+								end
+							end,
+						})
+					end)
 				end)
 			end,
 		})
